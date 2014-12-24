228755d269dd8ee596846a8e5af4f723ba1bbee7
mike <eleventynine@gmail.com>
2014-12-24 16:07:49 +1100

    delete Data.Index.Nat

--- src/Data/Index/Nat.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE CPP                  #-}
-{-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE IncoherentInstances  #-}
-{-# LANGUAGE KindSignatures       #-}
-{-# LANGUAGE UndecidableInstances #-}
-module Data.Index.Nat (cnat, module TypeLits) where
-import GHC.TypeLits as TypeLits
