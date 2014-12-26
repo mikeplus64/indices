b9a36ca8ddbe12a6fa4cbe397e1c916625f635e7
mike <eleventynine@gmail.com>
2014-12-26 14:03:12 +1100

    update for 1.7.0

--- benchmarks/Unroll.hs
+++ benchmarks/Unroll.hs
@@ -3,9 +3,13 @@
 {-# LANGUAGE QuasiQuotes   #-}
 {-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -fcontext-stack=100 #-}
+import Control.DeepSeq
 import Criterion.Main
 import Data.Index
 import Data.Proxy
+
+instance NFData Z
+instance NFData (x:.xs)
 
 {-# INLINE unrollRange #-}
 unrollRange :: Ranged a => Proxy a -> [a]
@@ -15,6 +19,7 @@ unrollRange _ = range Unroll
 unrollRangeNoinline :: Ranged a => Proxy a -> [a]
 unrollRangeNoinline _ = range Unroll
 
+{-# INLINE rollRange #-}
 rollRange :: Dim a => Proxy a -> [a]
 rollRange _ = range Roll
 
@@ -34,6 +39,7 @@ unrollWithRange r = withRange (unroll r) test
 unrollWithRangeNoinline :: Ranged n => Proxy n -> IO ()
 unrollWithRangeNoinline r = withRange (unroll r) test
 
+{-# INLINE rollWithRange #-}
 rollWithRange :: Dim n => Proxy n -> IO ()
 rollWithRange r = withRange (roll r) test
 
@@ -43,20 +49,16 @@ rollWithRangeNoinline r = withRange (roll r) test
 
 main :: IO ()
 main = defaultMain
-  [ bgroup "withRanges"
-    [ benchWithRanges [dim|2 2|]
-    , benchWithRanges [dim|4 4|]
-    ]
-  , bgroup "ranges"
-    [ benchRanges [dim|2 2|]
-    , benchRanges [dim|4 4|]
-    ]
+  [ benchWithRanges [dim|2 2|]
+  , benchWithRanges [dim|4 4|]
+  , benchRanges [dim|2 2|]
+  , benchRanges [dim|4 4|]
   ]
 
 {-# INLINE benchWithRanges #-}
-benchWithRanges :: (Show r, Ranged r) => Proxy r -> Benchmark
+benchWithRanges :: (Show r, NFData r, Ranged r) => Proxy r -> Benchmark
 benchWithRanges r
-  = bgroup (show (reflect `asProxyTypeOf` r))
+  = bgroup (showBound r)
     [ bench "rollWithRange"           . whnfIO $ rollWithRange r
     , bench "rollWithRangeNoinline"   . whnfIO $ rollWithRangeNoinline r
     , bench "unrollWithRange"         . whnfIO $ unrollWithRange r
@@ -64,11 +66,11 @@ benchWithRanges r
     ]
 
 {-# INLINE benchRanges #-}
-benchRanges :: (Show r, Ranged r) => Proxy r -> Benchmark
+benchRanges :: (Show r, NFData r, Ranged r) => Proxy r -> Benchmark
 benchRanges r
-  = bgroup (show (reflect `asProxyTypeOf` r))
-    [ bench "rollRange"            $ whnf rollRange r
-    , bench "rollRangeNoinline"    $ whnf rollRangeNoinline r
-    , bench "unrollRange"          $ whnf unrollRange r
-    , bench "unrollRangeNoinline"  $ whnf unrollRangeNoinline r
+  = bgroup (showBound r)
+    [ bench "rollRange"            $ nf rollRange r
+    , bench "rollRangeNoinline"    $ nf rollRangeNoinline r
+    , bench "unrollRange"          $ nf unrollRange r
+    , bench "unrollRangeNoinline"  $ nf unrollRangeNoinline r
     ]
